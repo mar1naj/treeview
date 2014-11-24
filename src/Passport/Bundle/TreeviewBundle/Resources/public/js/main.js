a29a414a43b16901c6cc97855097ec8b32c2972d
Marwan <mar1naj@hotmail.com>
2014-11-23 23:42:45 -0500

    asdf

--- src/Passport/Bundle/TreeviewBundle/Resources/public/js/main.js
+++ src/Passport/Bundle/TreeviewBundle/Resources/public/js/main.js
@@ -1,11 +1,8 @@
-function rightclick(ele){
-    
-    alert('this isd the elemsnet: '+ele);
-    
-    <div class="contextmenu">
-        <ul>
-            <li><a>Generate</a></li>
-            <li><a>Delete</a></li>
-        </ul>
-    </div>
-}
+$(document).on("contextmenu", ".rclickable", function(e){
+   $(".contextmenu").css( {display:"block", position:"absolute", top:event.pageY, left: event.pageX+20});
+   return false;
+});
+
+$('.rclickable').focusout(function(){
+    $(".contextmenu").css('display', 'none');
+});
